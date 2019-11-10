e3671b68d8c2cc3c5eb4d9ed2b323bcae8f5e2f9
srndo <xsesta06@stud.fit.vutbr.cz>
2019-11-10 23:33:05 +0100

    Cvičenie 3

---
+++ Cvičenie 3/Windows/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using WPFWeather.Services;
+using WPFWeather.ViewModels;
+
+namespace WPFWeather.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
