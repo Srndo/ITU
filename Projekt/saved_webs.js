495bbee35ab37e007717edd550d338e588e13ca3
Srndo <xsesta06@stud.fit.vutbr.cz>
2019-12-08 23:44:23 +0100

    autor commentary

--- Projekt/saved_webs.js
+++ Projekt/saved_webs.js
@@ -1,4 +1,4 @@
-// počká na načítanie dokumentu
+/*  Autor: Šimon Šesták (xsesta06) */
 document.addEventListener('DOMContentLoaded', (event) => {console.log('DOM fully loaded');}, false);
 // priradí k elementom funkciu
 document.getElementById('option1').addEventListener('click', set_active, false);
@@ -7,9 +7,9 @@ document.getElementById('option2').addEventListener('click', set_active, false);
 
 	// funkcia pre vizualne "aktivovanie po kliknuti" presetov
 	function set_active() {
-		// získa stalačený pre set
+		// získa stalačený preset
 		let activate = document.getElementById(event.target.id).parentElement;
-		// získa práve aktívny pre set
+		// získa práve aktívny preset
 		let remove = document.getElementsByClassName("active");
 		// deactivácia aktivovaného presetu
 		remove[0].classList.remove("active");
