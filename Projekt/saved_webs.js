18ae0dcd9ab289e7136cc1a20a3de2ed78336013
Srndo <xsesta06@stud.fit.vutbr.cz>
2019-12-09 13:32:05 +0100

    something

--- Projekt/saved_webs.js
+++ Projekt/saved_webs.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', (event) => {console.log('DOM fully
 document.getElementById('option1').addEventListener('click', set_active, false);
 document.getElementById('option3').addEventListener('click', set_active, false);
 document.getElementById('option2').addEventListener('click', set_active, false);
+document.getElementById('favorite_1').addEventListener('click', set_favorite, false);
+document.getElementById('favorite_2').addEventListener('click', set_favorite, false);
+document.getElementById('favorite_3').addEventListener('click', set_favorite, false);
+//document.getElementById('favorite_4').addEventListener('click', set_favorite, false);
 
 	// funkcia pre vizualne "aktivovanie po kliknuti" presetov
 	function set_active() {
@@ -16,3 +20,43 @@ document.getElementById('option2').addEventListener('click', set_active, false);
 		// aktivácia stlačeného presetu
 		activate.classList.add("active");
 	};
+
+	function set_favorite() {
+		
+		let get = document.getElementById(event.target.id);
+		let set;
+		switch (get.id)
+		{
+			case "favorite_1":
+				set = document.getElementById("star_1");
+				if(set.hasAttribute("checked"))
+					set.removeAttribute("checked");
+				else
+					set.setAttribute("checked", "");
+				break;
+
+			case 'favorite_2':
+				set = document.getElementById("star_2");
+				if(set.hasAttribute("checked"))
+					set.removeAttribute("checked");
+				else
+					set.setAttribute("checked", "");
+				break;
+
+			case 'favorite_3':
+				set = document.getElementById("star_3");
+				if(set.hasAttribute("checked"))
+					set.removeAttribute("checked");
+				else
+					set.setAttribute("checked", "");
+				break;
+
+			case 'favorite_4':
+				set = document.getElementById("star_4");
+				if(set.hasAttribute("checked"))
+					set.removeAttribute("checked");
+				else
+					set.setAttribute("checked", "");
+				break;
+		}
+	}
